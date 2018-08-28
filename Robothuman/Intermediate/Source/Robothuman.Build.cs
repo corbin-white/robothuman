1b524066c7f352eb353781220cf2f043adfbb008
Khadeidre Dean <khadeidredean@gmail.com>
2018-08-28 12:53:44 -0600

    Collect Lightning; Unlock Door; Lightning Damage; Spawn Enemies

---
+++ Robothuman/Intermediate/Source/Robothuman.Build.cs
@@ -0,0 +1,10 @@
+using UnrealBuildTool;
+
+public class Robothuman : ModuleRules
+{
+	public Robothuman(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
